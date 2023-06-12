6c402149cb56926f46cbdaf60d853d164584af86
Mihail <funkypickledpickle@gmail.com>
2023-06-12 21:45:45 +0300

    Added EntityGroupExtensions for enumerating specific components

---
+++ Assets/Scripts/3_GameplayECS/Extensions/EntityGroupExtensions.cs
@@ -0,0 +1,22 @@
+using Asteroids.ValueTypeECS.Components;
+using Asteroids.ValueTypeECS.EntityGroup;
+
+namespace Asteroids.GameplayECS.Extensions
+{
+    public delegate void ActionReference<TReferenced1, TReferenced2>(ref TReferenced1 referenced1, ref TReferenced2 referenced2) where TReferenced1 : struct where TReferenced2 : struct;
+
+    public static class EntityGroupExtensions
+    {
+        public static void ForEachOnlyComponents<TComponent1, TComponent2>(this EntityGroup entityGroup, ActionReference<TComponent1, TComponent2> actionReference) where TComponent1 : struct, IECSComponent where TComponent2 : struct, IECSComponent
+        {
+            var world = entityGroup.World;
+            foreach (var id in entityGroup)
+            {
+                ref var entity = ref world.GetEntity(id);
+                ref var component1 = ref entity.GetComponent<TComponent1>();
+                ref var component2 = ref entity.GetComponent<TComponent2>();
+                actionReference(ref component1, ref component2);
+            }
+        }
+    }
+}

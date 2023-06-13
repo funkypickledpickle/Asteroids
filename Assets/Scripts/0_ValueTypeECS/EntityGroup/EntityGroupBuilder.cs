2898797a7a4a49932a5933625da9a11c52edcb75
Mihail <funkypickledpickle@gmail.com>
2023-06-13 14:13:17 +0300

    Added EntityGroupBuilder to prevent the user from using lambda methods and to create inherited EntityGroup classes

---
+++ Assets/Scripts/0_ValueTypeECS/EntityGroup/EntityGroupBuilder.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Linq;
+using Asteroids.ValueTypeECS.Components;
+using Asteroids.ValueTypeECS.Delegates;
+using Asteroids.ValueTypeECS.Entities;
+using Asteroids.ValueTypeECS.EntityContainer;
+using Zenject;
+
+namespace Asteroids.ValueTypeECS.EntityGroup
+{
+    public class EntityGroupBuilder
+    {
+        [Inject] private readonly World _world;
+
+        private readonly List<FunctionReference<Entity, bool>> _conditions = new List<FunctionReference<Entity, bool>>();
+
+        public EntityGroupBuilder RequireComponent<TComponent>() where TComponent : struct, IECSComponent
+        {
+            _conditions.Add((ref Entity referenced) => referenced.HasComponent<TComponent>());
+            return this;
+        }
+
+        public EntityGroupBuilder RequireComponentAbsence<TComponent>() where TComponent : struct, IECSComponent
+        {
+            _conditions.Add((ref Entity referenced) => !referenced.HasComponent<TComponent>());
+            return this;
+        }
+
+        public EntityGroup Build()
+        {
+            return new EntityGroup(_world, new Matcher(_conditions.ToList()).CheckEntity);
+        }
+
+        private class Matcher
+        {
+            private readonly List<FunctionReference<Entity, bool>> _conditions;
+
+            public Matcher(List<FunctionReference<Entity, bool>> conditions)
+            {
+                _conditions = conditions;
+            }
+
+            public bool CheckEntity(ref Entity entity)
+            {
+                for (var i = 0; i < _conditions.Count; i++)
+                {
+                    if (!_conditions[i](ref entity))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}

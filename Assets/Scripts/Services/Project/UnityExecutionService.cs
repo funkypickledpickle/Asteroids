f29ccab45b3b0b9163ffe5eae9309990e29184d3
Mihail <funkypickledpickle@gmail.com>
2023-06-14 17:06:50 +0300

    Implemented FrameInfoService in UnityExecutionService

--- Assets/Scripts/Services/Project/UnityExecutionService.cs
+++ Assets/Scripts/Services/Project/UnityExecutionService.cs
@@ -18,22 +18,36 @@ namespace Asteroids.Services.Project
         void UnsubscribeFromUpdate(Action action);
     }
 
-    public class UnityExecutionService : MonoBehaviour, IUnityExecutionService
+    public class UnityExecutionService : MonoBehaviour, IFrameInfoService, IUnityExecutionService
     {
+        private const float DefaultTimeScale = 1;
+        private const float PausedTimeScale = 0;
+
         private Action _unityUpdateAction;
+
+        private float _lastStartTime;
+
+        public float DeltaTime { get; private set; }
+        public float StartTime { get; private set; }
 
         public void Update()
         {
+            _lastStartTime = StartTime;
+            StartTime = Time.time;
+            DeltaTime = StartTime - _lastStartTime;
+
             _unityUpdateAction?.Invoke();
         }
 
         public void PauseUpdate()
         {
+            Time.timeScale = PausedTimeScale;
             gameObject.SetActive(false);
         }
 
         public void UnPauseUpdate()
         {
+            Time.timeScale = DefaultTimeScale;
             gameObject.SetActive(true);
         }
 

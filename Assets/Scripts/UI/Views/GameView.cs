f178418efb793abc467c9c2412e7a2bf858def86
Mihail <funkypickledpickle@gmail.com>
2024-04-24 15:59:02 +0300

    Added score lable to game view to make score visible during game

--- Assets/Scripts/UI/Views/GameView.cs
+++ Assets/Scripts/UI/Views/GameView.cs
@@ -1,3 +1,4 @@
+using System;
 using Asteroids.Gameplay.States;
 using Asteroids.Installation;
 using Asteroids.GameplayECS.Components;
@@ -5,6 +6,7 @@ using Asteroids.GameplayECS.Extensions;
 using Asteroids.Services;
 using Asteroids.Tools;
 using Asteroids.UnsafeTools;
+using Asteroids.ValueTypeECS.Entities;
 using Asteroids.ValueTypeECS.EntityGroup;
 using TMPro;
 using UnityEngine;
@@ -22,17 +24,23 @@ namespace Asteroids.UI.Views
         [SerializeField] private TMP_Text _chargesCountLabel;
         [SerializeField] private TMP_Text _rechargeTimerLabel;
 
+        [SerializeField] private TMP_Text _scoreLabel;
+
         [Inject] private readonly IStateContext _stateContext;
         [Inject] private readonly IInstanceSpawner _instanceSpawner;
         [Inject] private readonly IFrameInfoService _frameInfoService;
 
         private EntityGroup _playerGroup;
+        private EntityGroup _scoreGroup;
+        private EntityGroup _receivedScoreGroup;
 
         private Vector2MutableStringPresenter _positionText = new Vector2MutableStringPresenter(NumericFormats.G4);
         private SingleMutableStringPresenter _rotationText = new SingleMutableStringPresenter(NumericFormats.G4);
         private SingleMutableStringPresenter _velocityText = new SingleMutableStringPresenter(NumericFormats.G4);
         private IntegerMutableStringPresenter _chargesCountText = new IntegerMutableStringPresenter(NumericFormats.G4);
         private SingleMutableStringPresenter _rechargeTimerText = new SingleMutableStringPresenter(NumericFormats.G4);
+
+        private IntegerMutableStringPresenter _scoreText = new IntegerMutableStringPresenter(new GenericFormat(10));
 
         private void Awake()
         {
@@ -44,12 +52,29 @@ namespace Asteroids.UI.Views
                 .RequireComponent<LaserGunComponent>()
                 .RequireComponent<LaserAutoChargingComponent>()
                 .Build();
+
+            _scoreGroup = _instanceSpawner.Instantiate<EntityGroupBuilder>().RequireComponent<ScoreComponent>().Build();
+
+            _receivedScoreGroup = _instanceSpawner.Instantiate<EntityGroupBuilder>().RequireComponent<ReceivedScoreComponent>().Build();
+            _receivedScoreGroup.EntityRemoved += HandleScoreGained;
+        }
+
+        private void OnEnable()
+        {
+            UpdateScore();
         }
 
         private void OnDestroy()
         {
             _playerGroup.Dispose();
             _playerGroup = null;
+
+            _scoreGroup.Dispose();
+            _scoreGroup = null;
+
+            _receivedScoreGroup.EntityRemoved -= HandleScoreGained;
+            _receivedScoreGroup.Dispose();
+            _receivedScoreGroup = null;
         }
 
         private void Update()
@@ -82,6 +107,18 @@ namespace Asteroids.UI.Views
             var timerValue = isLaserReady ? 0 : laserReadyTime - _frameInfoService.StartTime;
             _rechargeTimerText.UpdateContent(timerValue);
             _rechargeTimerLabel.SetText(_rechargeTimerText.ToString());
+        }
+
+        private void HandleScoreGained(ref Entity referenced)
+        {
+            UpdateScore();
+        }
+
+        private void UpdateScore()
+        {
+            ref var scoreComponent = ref _scoreGroup.GetFirst().GetComponent<ScoreComponent>();
+            _scoreText.UpdateContent(scoreComponent.Score);
+            _scoreLabel.SetText(_scoreText.ToString());
         }
     }
 }
